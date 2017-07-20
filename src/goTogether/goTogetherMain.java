f1549e79366d7cdfd3be2d12e6739d49c98886c7
YounHo <ultraclass@gmail.com>
2017-07-20 16:46:30 +0900

    Changed code
    Changed code

--- src/goTogether/goTogetherMain.java
+++ src/goTogether/goTogetherMain.java
@@ -2,10 +2,43 @@ package goTogether;
 
 public class goTogetherMain {
 
-	  public int test(int a, int b)
+	  public int setStartAutomaticallyt(int a, int b)
 	  {
 		  
 		  return a * b;
 	  }
+	  
+	  public int setDestByTouch(int a, int b)
+	  {
+		  
+		  return a * b;
+	  }
+	  
+	  public int saveFavCar(int a, int b)
+	  {
+		  
+		  return a * b;
+	  }
+	  
+	  public int createAccount(int a, int b)
+	  {
+		  
+		  return a * b;
+	  }
+	  
+	  
+	  public int savePhoneNum4Rider(int a, int b)
+	  {
+		  
+		  return a * b;
+	  }
+	  
+	  public int setFavRoute(int a, int b)
+	  {
+		  
+		  return a * b;
+	  }
+	  
+	
 	
 }
